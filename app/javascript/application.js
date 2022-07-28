0f7eb388ce30ec5d202172c7fbafda5474d64280
Ana Clara Mansano <anamansano@jera.com.br>
2022-07-28 14:51:03 -0300

    Atualização dos visuais da aplicação

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import 'bootstrap'
+import './src/application.scss'
 
 $(function () {
-    'use strict'
-  
-    $('[data-toggle="offcanvas"]').on('click', function () {
-      $('.offcanvas-collapse').toggleClass('open')
-    })
+  'use strict'
+
+  $('[data-toggle="offcanvas"]').on('click', function () {
+    $('.offcanvas-collapse').toggleClass('open')
+  })
 })
